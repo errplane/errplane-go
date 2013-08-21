ee403944aa8eb89f041ea3737b3d8fa929c90f0d
John Shahid <jvshahid@gmail.com>
2013-08-21 17:50:00 -0400

    add a failing test.

--- aggregator_test.go
+++ aggregator_test.go
@@ -51,6 +51,25 @@ func (s *ErrplaneAggregatorApiSuite) TearDownSuite(c *C) {
 	udpListener.Close()
 }
 
+func (s *ErrplaneAggregatorApiSuite) TestDoesNotOverrideUdpAddr(c *C) {
+	ep := newTestClient("app4you2love", "staging", "some_key")
+	ep.SetUdpAddr(udpListener.LocalAddr().(*net.UDPAddr).String())
+	ep.SetHttpHost("localhost") // there was a bug where this call will override the udp addr that the user sets
+	c.Assert(ep, NotNil)
+
+	err := ep.ReportUDP("some_metric", 123.4, "doesn't send empty points", Dimensions{
+		"foo": "bar",
+	})
+	c.Assert(err, IsNil)
+	ep.Close()
+
+	time.Sleep(200 * time.Millisecond)
+
+	c.Assert(udpRecorder.requests, HasLen, 1)
+	expected := fmt.Sprintf(`{"d":"app4you2lovestaging","a":"some_key","o":"r","w":[{"n":"some_metric","p":[{"v":123.4,"c":"doesn't send empty points","d":{"foo":"bar"}}]}]}`)
+	c.Assert(udpRecorder.requests, Contains, expected)
+}
+
 // the purpose of this test is to make sure that we don't send empty arrays
 // to the backend which we were doing.
 func (s *ErrplaneAggregatorApiSuite) TestDoesNotSendEmptyPoints(c *C) {
